a6348565b7b2b15989e9e950451de120f9677a33
Keeghan56 <121057424+Keeghan56@users.noreply.github.com>
2023-10-22 20:41:38 -0400

    ghj

---
+++ DDPG_Trial/run.py
@@ -0,0 +1,60 @@
+import sys
+import ddpg_tf
+from ddpg_tf import Agent
+sys.path.insert(0, "..")
+from opcua import Client
+import time
+import tensorflow as tf
+import numpy as np
+
+if __name__ == "__main__":
+    #Connect to the client
+    client = Client("opc.tcp://DESKTOP-A9QNR1L:4990/FactoryTalkLinxGateway1")
+    try:
+        client.connect()
+        root = client.get_root_node()
+        setpoint = client.get_node("ns=2;s=[opc_server]setpoint_diameter")
+        diameter = client.get_node("ns=2;s=[opc_server]Diameter")
+        setpoint_speed = client.get_node("ns=2;s=[opc_server]DC_output")
+        model = Agent.load_models('C:/Users/mit-f/Documents/model2/model1') #input the path address of the model
+
+        def get_state_from_system():
+            s = setpoint.get_value()
+            d = diameter.get_value()
+
+            d_means = 0.42954475
+            s_means = 0.40073333
+            d_std = 0.13086448
+            s_std = 0.16299119 
+
+            d_normalized = (d-d_means)/d_std
+            s_normalized = (s-s_means)/s_std
+
+            return [s_normalized, d_normalized]
+        
+        def send_action_to_system(action):
+            rpm = setpoint_speed.set_value(action)
+
+        while True:
+            try:
+                current_state = get_state_from_system()
+                action = Agent.choose_action(current_state)
+
+                #Limiting rpm range to 15-200
+                if action <= 15:
+                    send_action_to_system(15)
+                elif action >= 200:
+                    send_action_to_system(200)
+                else:
+                    send_action_to_system(float(action))
+
+                time.sleep(0.05)
+
+            except KeyboardInterrupt:
+                print("Keyboard interrupt detected Exiting...")
+                break
+    finally:
+        client.disconnect()
+
+        
+        
---
+++ DDPG_Trial/train.py
@@ -0,0 +1,112 @@
+import pandas as py 
+import ddpg_tf
+from ddpg_tf import Agent
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Intialize DDPG Agent
+agent = Agent(alpha=0.00005, beta=0.0005, input_dims=[2], tau=0.001,
+              batch_size=64, layer1_size=800, layer2_size=600,
+              n_actions=1)
+
+#Importing the DataSet
+
+csv_file_path_1 = 'E:/grad/thesis/8-18 Experiments/diameter&RPM_SP0.2.CSV'  # Replace with the actual path to your Excel file
+df_1 = pd.read_csv(csv_file_path_1, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
+df_1['Setpoint'] = 0.2
+
+csv_file_path_2 = 'E:/grad/thesis/8-18 Experiments/diameter&RPM_SP0.4.CSV'  # Replace with the actual path to your Excel file
+df_2 = pd.read_csv(csv_file_path_2, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
+df_2['Setpoint'] = 0.4
+
+csv_file_path_3 = 'E:/grad/thesis/8-18 Experiments/diameter&RPM_SP0.6.CSV'  # Replace with the actual path to your Excel file
+df_3 = pd.read_csv(csv_file_path_3, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
+df_3['Setpoint'] = 0.6
+
+#Combine DataSet 
+features = pd.concat([df_1, df_2, df_3], axis=0)
+
+# Assume X_train, y_train, X_test, y_test are your data
+X_train_o, X_test_o, y_train, y_test = train_test_split(features[['Diameter','Setpoint']],features[['Measured_rpm']], test_size=0.2, random_state=42)
+
+#Normalize the Data
+scaler = preprocessing.StandardScaler().fit(X_train_o)
+X_train = scaler.transform(X_train_o)
+scaler_2 = preprocessing.StandardScaler().fit(X_test_o)
+X_test = scaler_2.transform(X_test_o)
+
+#Training Loop
+num_epochs = 15000
+X_train = pd.DataFrame(X_train)
+y_train = pd.DataFrame(y_train)
+score_history = []
+
+for epoch in range(num_epochs):
+    total_reward = 0
+    for state, action in zip(X_train.values, y_train.values):
+        #Add state and action to an array
+        state = np.array([state])
+        action = np.array([action])
+
+        #Predict the next state 
+        next_state = Agent.choose_action(state)
+
+        #Calculate the reward [Note this reward function may need to be corrected]
+        reward = -abs(X_train-state)
+
+        #Store the experience
+        Agent.remember(state, action, reward, next_state)
+
+        #Learn from the experience
+        Agent.learn()
+
+        total_reward += reward
+
+    score_history.append(total_reward)
+print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')
+
+mse = mean_squared_error(y_train, action)
+print ("Mean Squared Error: ", mse)
+
+#Testing Loop
+num_epochs_testing = 15000
+X_test = pd.DataFrame(X_test)
+y_test = pd.DataFrame(y_test)
+score_history_testing = []
+
+for epoch in range(num_epochs):
+    total_reward = 0
+    for state, action in zip(X_test.values, y_test.values):
+        #Add state and action to an array
+        state = np.array([state])
+        action = np.array([action])
+
+        #Predict the next state 
+        next_state = Agent.choose_action(state)
+
+        #Calculate the reward [Note this reward function may need to be corrected]
+        reward = -abs(X_test-state)
+
+        #Store the experience
+        Agent.remember(state, action, reward, next_state)
+
+        #Learn from the experience
+        Agent.learn()
+
+        total_reward += reward
+
+    score_history.append(total_reward)
+print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')
+
+mse = mean_squared_error(y_test, action)
+print ("Mean Squared Error: ", mse)
+
+#Save the model
+Agent.save_models()
