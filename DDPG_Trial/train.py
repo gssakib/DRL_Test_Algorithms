68c19677a5ef03107ccbcc17a446f2e5b6fb302e
Keeghan56 <121057424+Keeghan56@users.noreply.github.com>
2023-11-05 15:34:53 -0500

    3

--- DDPG_Trial/train.py
+++ DDPG_Trial/train.py
@@ -19,15 +19,15 @@ agent = Agent(alpha=0.00005, beta=0.0005, input_dims=[2], tau=0.001,
 #Importing the DataSet
 
 csv_file_path_1 = "C:/Users/keegh/Documents/Orbtronics Agri Sensor/DRL_Test_Algorithms/diameter&RPM_SP0.2.CSV" # Replace with the actual path to your Excel file
-df_1 = pd.read_csv(csv_file_path_1, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, including the specified column
+df_1 = pd.read_csv(csv_file_path_1, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
 df_1['Setpoint'] = 0.2
 
 csv_file_path_2 = "C:/Users/keegh/Documents/Orbtronics Agri Sensor/DRL_Test_Algorithms/diameter&RPM_SP0.4.CSV"  # Replace with the actual path to your Excel file
-df_2 = pd.read_csv(csv_file_path_2, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, including the specified column
+df_2 = pd.read_csv(csv_file_path_2, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
 df_2['Setpoint'] = 0.4
 
-csv_file_path_3 = "C:/Users/keegh/Documents/Orbtronics Agri Sensor/DRL_Test_Algorithms/diameter&RPM_SP0.6.CSV"  # Replace with the actual path to your Excel file
-df_3 = pd.read_csv(csv_file_path_3, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, including the specified column
+csv_file_path_3 = "C:/Users/keegh/Documents/Orbtronics Agri Sensor/DRL_Test_Algorithms/diameter&RPM_SP0.4.CSV"  # Replace with the actual path to your Excel file
+df_3 = pd.read_csv(csv_file_path_3, skiprows=13, header=0, usecols=[3,4], nrows=5000) # Load the Excel sheet, excluding the specified column
 df_3['Setpoint'] = 0.6
 
 #Combine DataSet 
@@ -36,20 +36,22 @@ features = pd.concat([df_1, df_2, df_3], axis=0)
 # Assume X_train, y_train, X_test, y_test are your data
 X_train_o, X_test_o, y_train, y_test = train_test_split(features[['Diameter','Setpoint']],features[['Measured_rpm']], test_size=0.2, random_state=42)
 
-
 #Normalize the Data
 scaler = preprocessing.StandardScaler().fit(X_train_o)
 X_train = scaler.transform(X_train_o)
 scaler_2 = preprocessing.StandardScaler().fit(X_test_o)
 X_test = scaler_2.transform(X_test_o)
 
-
 #Training Loop
+<<<<<<< HEAD
 <<<<<<< HEAD
 num_epochs = 1
 =======
 num_epochs = 10
 >>>>>>> parent of 38b306a (sadsfd)
+=======
+num_epochs = 5
+>>>>>>> parent of 066a6ba (fghjk)
 X_train = pd.DataFrame(X_train)
 y_train = pd.DataFrame(y_train)
 score_history = []
@@ -63,8 +65,8 @@ for epoch in range(num_epochs):
         state = np.array(state)
         state = np.array(state, dtype=np.float32)
         #state = (state, {})
-        #print("The state is", state)
         action = np.array([action])
+<<<<<<< HEAD
         #print("The action is", action)
         predicted_actions = np.array([predicted_actions])
         predicted_action = np.array([predicted_actions])
@@ -73,10 +75,14 @@ for epoch in range(num_epochs):
         predicted_action = agent.choose_action(state)
         predicted_actions.append(predicted_action)
         #print("The prediction of the next state is", next_state)
+=======
+                
+        #Predict the next state 
+        next_state = agent.choose_action(state)
+>>>>>>> parent of 066a6ba (fghjk)
 
         #Calculate the reward [Note this reward function may need to be corrected]
         reward = -abs(state[0]-state[1]) #diameter - setpoint
-        #print("The reward is ", state[0], "-", state[1], "=", reward)
 
         #Store the experience
         agent.remember(state, action, reward, predicted_action)
@@ -88,6 +94,7 @@ for epoch in range(num_epochs):
         counter += 1
         print(counter)
 
+<<<<<<< HEAD
         #Calculate MSE using predicted action and true actions
         predicted_actions = np.full(y_train.shape, predicted_actions)
         mse= mean_squared_error(y_train, predicted_actions)
@@ -99,12 +106,20 @@ score_history.append(total_reward)
 #mse = mean_squared_error(y_train, action)
 #print ("Mean Squared Error: ", mse)
 #print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')   
+=======
+    score_history.append(total_reward)
+print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')
+>>>>>>> parent of 066a6ba (fghjk)
 
 #print(y_train.shape)
 #print(action.shape)
 
+action = np.full(y_train.shape, action)
+mse = mean_squared_error(y_train, action)
+print ("Mean Squared Error: ", mse)
+
 #Testing Loop
-num_epochs_testing = 1
+num_epochs_testing = 5
 X_test = pd.DataFrame(X_test)
 y_test = pd.DataFrame(y_test)
 score_history_testing = []
@@ -137,6 +152,7 @@ for epoch in range(num_epochs):
         counter += 1
         print(counter)
 
+<<<<<<< HEAD
         #Calculate MSE using predicted action and true actions
         predicted_actions = np.full(y_train.shape, predicted_actions)
         mse= mean_squared_error(y_train, predicted_actions)
@@ -149,6 +165,14 @@ for epoch in range(num_epochs):
 #print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')
 #mse = mean_squared_error(y_test, action)
 #print ("Mean Squared Error: ", mse)
+=======
+    score_history.append(total_reward)
+    print(f'Epoch {epoch+1}/{num_epochs}, Total Reward: {total_reward}')
+    mse = mean_squared_error(y_test, action)
+    print ("Mean Squared Error: ", mse)
+
+action = np.full(y_test.shape, action)
+>>>>>>> parent of 066a6ba (fghjk)
 
 #Save the model
 agent.save_models()
